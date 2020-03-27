3da1f9d87dcdeaedb9f5969e680aef25480de60b
unknown <yerzxe1@naver.com>
2020-03-27 16:39:09 +0900

    Release commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +21,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    */
+    <p>Github Release</p>
+  )
 }
 
-export default App;
+export default App
